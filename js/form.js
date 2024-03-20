baf13b80b171a85dfd178a3e9891e70dc5efe1a7
Fabien Jacquemot <fabien.jacquemot1@gmail.com>
2024-03-20 09:40:49 +0100

    Form done - Jobs cards

--- js/form.js
+++ js/form.js
@@ -1,15 +1,19 @@
 
+
+
 const openModalButton = document.querySelector('#open-modal');
 const modal = document.querySelector('#myModal');
+const modalBackground = document.querySelector('#modalBg')
 
  openModalButton.addEventListener('click', function() {
         modal.style.display = "block";
-        modal.style.backdropFilter = "red";
-        console.log("OK")
+        modalBackground.style.display = "block"
+        modalBackground.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
     });
 
 window.addEventListener('click', function(event) {
-        if (event.target === modal) {
+        if (event.target === modalBackground) {
             modal.style.display = "none";
+            modalBackground.style.display = "none"
         }
     });
